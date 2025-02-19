98845bd21f8aab2f2821f9a33400eaac003911ae
Guilherme Rocha <nrocha.guilherme@gmail.com>
2025-02-19 20:46:27 -0300

    Guilherme - Alterar o arquivo main para ficar de acordo com as telas externas a ele

--- main.dart
+++ main.dart
@@ -1,10 +1,21 @@
-import 'package:english_words/english_words.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+import 'bibliotecas.dart';
+import 'Telas/utilidades.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MyApp(),
+    ),
+  );
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,39 +28,42 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 31, 81, 157)),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 188, 185, 225),
+          ),
         ),
-        home: MyHomePage(),
+        initialRoute: '/login', // Define a rota inicial
+        routes: {
+          '/login': (context) => LoginPage(),
+          '/cadastro': (context) => CadastroPage(),
+          '/home': (context) => MyHomePage(),
+          '/criacao': (context)=>ProfileCreationPage(),
+          '/reset':(context)=>SenhaPage(),
+          '/apagar':(context)=>DeleteAccountPage(),
+          '/apagarperfil':(context)=>DeleteProfilePage(),
+          '/quiz':(context)=>QuizPage()
+        },
       ),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
 
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
 
-  // ↓ Add the code below.
-  var favorites = <WordPair>[];
+// Tela de Login
+// Tela de Login
+// Importações necessárias
+// Classe para interagir com o Firebase
 
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-}
 
-// ...
- 
 
-// ...
+
+
+// Tela de Cadastro
+
+
+
+
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -57,271 +71,123 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var selectedindex=0;
-  @override
-  Widget build(BuildContext context) {
-  Widget page=Placeholder();
-  switch (selectedindex) {
-  case 0:
-    page = GeneratorPage();
-    break;
-  case 1:
-    page = FavoritesPage();
-    break;
-  
-}
+  var selectedindex = 0;
+  bool showNavigationRail = true; // Controle de visibilidade da aba
+  List<String> recomendacoes = [];
 
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
-                ),
-              ],
-              selectedIndex: selectedindex,
-              onDestinationSelected: (value) {
-                setState(() {
-                  selectedindex=value;
-                });
-                
-              },
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.secondaryContainer,
-              child: page,
-            ),
-          ),
-        ],
-      ),
-    );
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // Pegando as recomendações da tela anterior
+    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    if (args != null) {
+      setState(() {
+        recomendacoes = args['recomendacoes'] ?? [];
+      });
+    }
   }
-}
-
-class GeneratorPage extends StatefulWidget {
-  @override
-  State<GeneratorPage> createState() => _GeneratorPageState();
-}
-
-class _GeneratorPageState extends State<GeneratorPage> {
-  String searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    // Filtrar pares com base na pesquisa
-    var isVisible = searchQuery.isEmpty ||
-        pair.asLowerCase.contains(searchQuery.toLowerCase());
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
+    Widget page = Placeholder();
+    switch (selectedindex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      case 2:
+        page = Profilels();
+        break;
+      case 3:
+        page = NotesPage();
+        break;
+      case 4:
+        page = Pesquisa();
+        break;
+      case 5:
+        page = QuizPage();  // Alterado para manter a navegação para a tela do Quiz
+        break;
+      case 6:
+        page = MapaJogosScreen();
+        break;
     }
 
-    return Column(
-      children: [
-        // Barra de pesquisa
-        Container(
-          width: double.infinity,
-          height: 60,
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 188, 185, 225),
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withValues(),
-                spreadRadius: 1,
-                blurRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Pesquisar par de palavras...',
-              border: InputBorder.none,
-              prefixIcon: Icon(Icons.search),
-            ),
-            onChanged: (value) {
-              setState(() {
-                searchQuery = value;
-              });
-            },
-          ),
-        ),
-        // Exibir par de palavras ou mensagem
-        PALAVRAS(isVisible: isVisible, pair: pair, appState: appState, icon: icon),
-      ],
-    );
-  }
-}
-
-class PALAVRAS extends StatelessWidget {
-  const PALAVRAS({
-    super.key,
-    required this.isVisible,
-    required this.pair,
-    required this.appState,
-    required this.icon,
-  });
-
-  final bool isVisible;
-  final WordPair pair;
-  final MyAppState appState;
-  final IconData icon;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: isVisible
-          ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  BIGcard(pair: pair),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          appState.toggleFavorite();
-                        },
-                        icon: Icon(icon),
-                        label: Text('Like'),
-                      ),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                        onPressed: () {
-                          appState.getNext();
-                        },
-                        child: Text('Next'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          : Center(
-              child: Text('Nenhum par de palavras encontrado.'),
-            ),
-    );
-  }
-}
-
-
-// ...
-
-class BIGcard extends StatelessWidget {
-  const BIGcard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    var theme=Theme.of(context);
-    var style=theme.textTheme.displayMedium!.copyWith(color:theme.colorScheme.onPrimary,);
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase,style: style,
-        semanticsLabel: pair.asPascalCase,),
-      ),
-    );
-  }
-}
-// ...
-
-class FavoritesPage extends StatefulWidget {
-  @override
-  State<FavoritesPage> createState() => _FavoritesPageState();
-}
-
-class _FavoritesPageState extends State<FavoritesPage> {
-  String searchQuery = '';
-
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    // Filtrar favoritos com base na pesquisa
-    var filteredFavorites = appState.favorites
-        .where((pair) => pair.asLowerCase.contains(searchQuery.toLowerCase()))
-        .toList();
-
-    return Column(
-      children: [
-        // Barra de pesquisa
-        Container( width: double.infinity, // Ocupa toda a largura disponível
-                   height: 60, // Define a altura
-                   padding: EdgeInsets.symmetric(horizontal: 10),
-                   decoration: BoxDecoration(
-                         color: const Color.fromARGB(255, 188, 185, 225),
-                         borderRadius: BorderRadius.circular(15),
-                         boxShadow: [
-                           BoxShadow(
-                                 color: Colors.grey.withValues(),
-                                 spreadRadius: 1,
-                                 blurRadius: 5,
-                                 offset: Offset(0, 3), // Sombra com deslocamento
-                              ),
-                            ],
+    return Scaffold(
+      body: GestureDetector(
+        onPanUpdate: (details) {
+          // Detecta o gesto de deslizar para a direita para abrir a barra
+          if (details.delta.dx > 0 && !showNavigationRail) {
+            setState(() {
+              showNavigationRail = true;
+            });
+          }
+          // Detecta o gesto de deslizar para a esquerda para fechar a barra
+          if (details.delta.dx < 0 && showNavigationRail) {
+            setState(() {
+              showNavigationRail = false;
+            });
+          }
+        },
+        child: Row(
+          children: [
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              width: showNavigationRail ? 72.0 : 0.0, // Largura do NavigationRail
+              child: showNavigationRail
+                  ? SafeArea(
+                      child: NavigationRail(
+                        destinations: [
+                          NavigationRailDestination(
+                            icon: Icon(Icons.home),
+                            label: Text('Home'),
                           ),
-          
-         
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Pesquisar favoritos...',
-              border: InputBorder.none,
-              prefixIcon: Icon(Icons.search),
-            ),
-            onChanged: (value) {
-              setState(() {
-                searchQuery = value;
-              });
-            },
-          ),
-        ),
-        // Lista de favoritos
-        Expanded(
-          child: filteredFavorites.isEmpty
-              ? Center(
-                  child: Text('Nenhum favorito encontrado.'),
-                )
-              : ListView(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Text('Você tem ${filteredFavorites.length} favoritos:'),
-                    ),
-                    for (var pair in filteredFavorites)
-                      ListTile(
-                        leading: Icon(Icons.favorite),
-                        title: Text(pair.asLowerCase),
+                          NavigationRailDestination(
+                            icon: Icon(Icons.favorite),
+                            label: Text('Favorites'),
+                          ),
+                          NavigationRailDestination(
+                            icon: Icon(Icons.person),
+                            label: Text('Perfil'),
+                          ),
+                          NavigationRailDestination(
+                            icon: Icon(Icons.note_alt_rounded),
+                            label: Text('Jogos'),
+                          ),
+                          NavigationRailDestination(
+                            icon: Icon(Icons.search),
+                            label: Text('Pesquisa'),
+                          ),
+                          NavigationRailDestination(
+                            icon: Icon(Icons.quiz),
+                            label: Text('quiz'),
+                          ),
+                          NavigationRailDestination(
+                            icon: Icon(Icons.map_outlined),
+                            label: Text('Mapa'),
+                          ),
+                        ],
+                        selectedIndex: selectedindex,
+                        onDestinationSelected: (value) {
+                          setState(() {
+                            selectedindex = value;
+                          });
+                        },
                       ),
-                  ],
-                ),
+                    )
+                  : null,
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.secondaryContainer,
+                child: page,
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
